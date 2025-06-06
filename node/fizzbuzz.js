0537696657525577c88126cb3a3876efc8369f4c
erii-01 <ganchozo.erika@gmail.com>
2025-06-06 13:08:46 -0300

    solución FizzBuzz + tests en Node

--- node/fizzbuzz.js
+++ node/fizzbuzz.js
@@ -1,5 +1,21 @@
 function fizzbuzz(n) {
-  return n;
+    if (n === 0) {
+        return 0;
+    }
+
+    if (n % 3 === 0 && n % 5 === 0) {
+        return 'FizzBuzz';
+    }
+
+    if (n % 3 === 0) {
+        return 'Fizz';
+    }
+
+    if (n % 5 === 0) {
+        return 'Buzz';
+    }
+
+    return n;
 }
 
 module.exports = fizzbuzz
