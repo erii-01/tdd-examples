86e25f6e067195c2908b4a67462ba22717b64bd6
erii-01 <ganchozo.erika@gmail.com>
2025-06-06 13:35:07 -0300

    solución FizzBuzz + tests en Ruby

--- ruby/fizzbuzz.rb
+++ ruby/fizzbuzz.rb
@@ -1,5 +1,19 @@
 def fizzbuzz(number)
-  return 'Fizz' if ! number.zero? && (number % 3).zero?
+    if number.zero?
+        return 0
+    end
 
-  number
+    if (number % 3).zero? && (number % 5).zero?
+        return 'FizzBuzz'
+    end
+  
+    if (number % 3).zero?
+        return 'Fizz'
+    end
+
+    if (number % 5).zero?
+        return 'Buzz'
+    end
+
+    number 
 end
