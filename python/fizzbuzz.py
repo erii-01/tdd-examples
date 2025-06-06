99dddaf97b1a11df4fdea779b2168daa9e4a6e50
erii-01 <ganchozo.erika@gmail.com>
2025-06-06 12:56:06 -0300

    solución FizzBuzz + tests en python

--- python/fizzbuzz.py
+++ python/fizzbuzz.py
@@ -1,6 +1,13 @@
 def fizzbuzz(n):
-    if n != 0 and n % 3 == 0: 
-       return 'Fizz'
-    return n
+    if n == 0:
+        return 0
+    elif n % 3 == 0 and n % 5 == 0:
+        return 'FizzBuzz'
+    elif n % 3 == 0:
+        return 'Fizz'
+    elif n % 5 == 0:
+        return 'Buzz'
+    else:
+        return n
 
 
--- python/test_fizzbuzz.py
+++ python/test_fizzbuzz.py
@@ -1,7 +1,6 @@
 import pytest
 from fizzbuzz import fizzbuzz
 
-
 def test_with_zero():
     assert fizzbuzz(0) == 0
 
@@ -9,16 +8,27 @@ def test_same_number_with_1_2_and_4():
     assert fizzbuzz(1) == 1
     assert fizzbuzz(2) == 2
     assert fizzbuzz(4) == 4
+    assert fizzbuzz(7) == 7 
+    assert fizzbuzz(11) == 11
 
 def test_print_Fizz_when_multiple_of_3_only():
     assert fizzbuzz(3) == 'Fizz'
+    assert fizzbuzz(6) == 'Fizz'
+    assert fizzbuzz(9) == 'Fizz'
+    assert fizzbuzz(12) == 'Fizz' 
+    assert fizzbuzz(18) == 'Fizz'
+    assert fizzbuzz(21) == 'Fizz'
     assert fizzbuzz(15) != 'Fizz'
-    pytest.fail('Add more tests for Fizz')
 
 def test_print_Fizz_when_multiple_of_5_only():
-    assert fizzbuzz(5) == 'Buzz'
+    assert fizzbuzz(5) == 'Buzz' 
+    assert fizzbuzz(10) == 'Buzz'
+    assert fizzbuzz(20) == 'Buzz'
+    assert fizzbuzz(25) == 'Buzz'
     assert fizzbuzz(15) != 'Buzz'
-    pytest.fail('Add more tests for Buzz')
 
 def test_print_Fizz_when_multiple_of_3_and_5():
-    pytest.fail('To be done')
+    assert fizzbuzz(15) == 'FizzBuzz'
+    assert fizzbuzz(30) == 'FizzBuzz'
+    assert fizzbuzz(45) == 'FizzBuzz'
+    assert fizzbuzz(0) != 'FizzBuzz'
