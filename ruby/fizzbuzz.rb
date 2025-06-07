c42742fb3dd228dae400e6fadaa91c2e794fccf8
erii-01 <ganchozo.erika@gmail.com>
2025-06-07 17:31:42 -0300

    Ruby: fizzbuzz.rb y test_fizzbuzz.rb modificados

--- ruby/fizzbuzz.rb
+++ ruby/fizzbuzz.rb
@@ -1,20 +1,12 @@
 def fizzbuzz(number)
-    if number.zero?
-        return 0
-    end
+  return 0 if number.zero?
 
-    if (number % 3).zero? && (number % 5).zero?
-        return 'FizzBuzz'
-    end
-  
-    if (number % 3).zero?
-        return 'Fizz'
-    end
+  return 'FizzBuzz' if (number % 3).zero? && (number % 5).zero?
 
-    if (number % 5).zero?
-        return 'Buzz'
-    end
+  return 'Fizz' if (number % 3).zero?
 
-    number 
+  return 'Buzz' if (number % 5).zero?
+
+  number
 end
 
